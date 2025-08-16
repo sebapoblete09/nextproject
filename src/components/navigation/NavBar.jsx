e11f0eb74c86c40036b98dac7a4ae1df2b7cdb6e
Sebastian Poblete <pseba9006@gmail.com>
2025-08-15 21:56:42 -0400

    Se agrego el navbar para movil

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -1,12 +1,28 @@
+"use client"
 import Link from "next/link";
+import { useState } from "react";
 import "./navbar.css"
 function NavBar(){
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <nav className="navbar py-5 gap-10">
+
+            {/* Logo */}
             <Link  href="/" className="navigation">
                 <h1 className="text-3xl font-bold">CrunchyClon</h1>
             </Link>
-            <ul>
+
+            {/* Boton version movil */}
+            <button className="hamburger"
+            onClick={() => setIsOpen(!isOpen)}
+            aria-label="Toggle menu">
+                ☰
+            </button>
+            
+            {/*Menu*/}
+            <ul className={`menu ${isOpen ? "open" : ""}`}>
                 <li>
                     <Link className="navigation hover:bg-gray-700 p-7" href="/">Inicio</Link>
                 </li>
